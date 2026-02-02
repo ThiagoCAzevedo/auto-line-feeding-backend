b52dce896bbb7c01479f7abefcb61d56d5486a18
Thiago Canato de Azevedo <thiago.azevedo@gruposese.com>
2026-02-02 09:21:05 -0300

    feat(orchestrators): created main orchestrator and files to separate workers and pipelines

---
+++ orchestrator/pipeline_registry.py
@@ -0,0 +1,11 @@
+from services.pipelines.pkmc_pk05.pipeline import pkmc_pk05_pipeline
+
+"""
+PIPELINES:
+- PKMC E PK05 - OK
+- 
+"""
+
+PIPELINES = {
+    "pkmc_pk05": pkmc_pk05_pipeline,
+}
---
+++ orchestrator/workers_registry.py
@@ -0,0 +1,14 @@
+
+# from services.workers.monitor_pkmc import PKMCMonitorService
+
+
+"""
+WORKERS:
+- Leitura da linha de montagem final
+- Leitura da linha de pintura
+- Verificação do saldo LB
+- Acesso ao SAP para retornar LT23
+"""
+
+WORKERS = {
+}

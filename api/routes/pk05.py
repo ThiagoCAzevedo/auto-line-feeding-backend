1f6809656fe7b05408db2cf8fb710cdfe9227c06
Thiago Canato de Azevedo <thiago.azevedo@gruposese.com>
2026-02-06 15:38:50 -0300

    feat(api): created api to pk05

---
+++ api/routes/pk05.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter, Query, Depends
+
+from database.queries import UpsertInfos
+from services.pk05.pk05 import PK05_Cleaner, PK05_DefineDataframe
+from helpers.services.pk05 import BuildPipeline, DependenciesInjection
+from helpers.services.http_exception import HTTP_Exceptions
+
+
+router = APIRouter()
+
+
+@router.get("/response/raw", summary="Get Raw PK05 Values")
+def get_raw_pk05(
+    svc: PK05_DefineDataframe = Depends(DependenciesInjection.get_pk05),
+    limit: int = Query(50, ge=1, le=1000),
+):
+    try:
+        df = svc.create_df().collect()
+        return df.head(limit).to_dicts()
+
+    except Exception as e:
+        raise HTTP_Exceptions().http_502("Erro ao buscar PK05 bruto: ", e)
+
+
+@router.get("/response/processed", summary="Get Cleaned PK05 Values")
+def get_clean_pk05(
+    raw_svc: PK05_DefineDataframe = Depends(DependenciesInjection.get_pk05),
+    cleaner_svc: PK05_Cleaner = Depends(DependenciesInjection.get_pk05_cleaner),
+    limit: int = Query(50, ge=1, le=1000),
+):
+    try:
+        df = (BuildPipeline.build_pk05(raw_svc, cleaner_svc).head(limit).collect())
+        return df.to_dicts()
+    except Exception as e:
+        raise HTTP_Exceptions().http_502("Erro ao processar PK05 (clean): ", e)
+
+
+@router.post("/upsert", summary="Upsert PK05 Values In The DataBase")
+def upsert_pk05(
+    batch_size: int = Query(10_000, ge=1, le=100_000),
+    raw_svc: PK05_DefineDataframe = Depends(DependenciesInjection.get_pk05),
+    cleaner_svc: PK05_Cleaner = Depends(DependenciesInjection.get_pk05_cleaner),
+    upsert_svc: UpsertInfos = Depends(DependenciesInjection.get_upsert_service),
+):
+    try:
+        df = BuildPipeline.build_pk05(raw_svc, cleaner_svc)
+        rows = upsert_svc.upsert_df("pk05", df, batch_size)
+
+        return {
+            "message": "Upsert PK05 concluído com sucesso.",
+            "rows": rows,
+            "batch_size": batch_size,
+            "table": "pk05",
+        }
+
+    except Exception as e:
+        raise HTTP_Exceptions().http_500("Erro no upsert (pk05)", e)
--- api/web.py
+++ api/web.py
@@ -6,6 +6,8 @@ from .routes.forecast import router as forecast_router
 from .routes.consumption import router as consumption_router
 from .routes.files import router as static_files_router
 from .routes.pkmc import router as pkmc_router
+from .routes.pk05 import router as pk05_router
+
 
 
 app = FastAPI(
@@ -19,4 +21,5 @@ app.include_router(assembly_router, prefix="/assembly", tags=["assembly"])
 app.include_router(forecast_router, prefix="/forecast", tags=["forecast"])
 app.include_router(consumption_router, prefix="/consumption", tags=["consumption"])
 app.include_router(pkmc_router, prefix="/pkmc", tags=["pkmc"])
+app.include_router(pk05_router, prefix="/pk05", tags=["pk05"])
 app.include_router(static_files_router, prefix="/files", tags=["files"])

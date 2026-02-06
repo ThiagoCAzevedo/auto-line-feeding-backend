0da3590e319a8b0ad45bce2bc63baf7c5bd8da44
Thiago Canato de Azevedo <thiago.azevedo@gruposese.com>
2026-02-06 15:26:18 -0300

    fix(pkmc): changed some problems with upsert route pkmc

--- api/routes/pkmc.py
+++ api/routes/pkmc.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Query, Depends
 
 from database.queries import UpsertInfos
 from services.pkmc.pkmc import PKMC_DefineDataframe, PKMC_Cleaner
-from helpers.services.pkmc import DependenciesInjection
+from helpers.services.pkmc import BuildPipeline, DependenciesInjection
 from helpers.services.http_exception import HTTP_Exceptions
 
 
@@ -22,33 +22,29 @@ def get_raw_pkmc(
         raise HTTP_Exceptions().http_502("Erro ao buscar PKMC bruto: ", e)
 
 
-@router.get("/respose/processed", summary="Get Cleaned PKMC Values")
+@router.get("/response/processed", summary="Get Cleaned PKMC Values")
 def get_clean_pkmc(
-    df_svc: PKMC_DefineDataframe = Depends(DependenciesInjection.get_pkmc),
+    raw_svc: PKMC_DefineDataframe = Depends(DependenciesInjection.get_pkmc),
     cleaner_svc: PKMC_Cleaner = Depends(DependenciesInjection.get_pkmc_cleaner),
     limit: int = Query(50, ge=1, le=1000),
 ):
     try:
-        df_raw = df_svc.create_df()
-        df_clean = cleaner_svc.clean(df_raw).collect()
-        return df_clean.head(limit).to_dicts()
-
+        df = (BuildPipeline.build_pkmc(raw_svc, cleaner_svc).head(limit).collect())
+        return df.to_dicts()
     except Exception as e:
         raise HTTP_Exceptions().http_502("Erro ao processar PKMC (clean): ", e)
 
 
-@router.post("/upsert", summary="Upsert Clean PKMC Values Into The Database")
-def upsert_clean_pkmc(
+@router.post("/upsert", summary="Upsert PKMC Values In The DataBase")
+def upsert_pkmc(
     batch_size: int = Query(10_000, ge=1, le=100_000),
-    df_svc: PKMC_DefineDataframe = Depends(DependenciesInjection.get_pkmc),
+    raw_svc: PKMC_DefineDataframe = Depends(DependenciesInjection.get_pkmc),
     cleaner_svc: PKMC_Cleaner = Depends(DependenciesInjection.get_pkmc_cleaner),
     upsert_svc: UpsertInfos = Depends(DependenciesInjection.get_upsert_service),
 ):
     try:
-        df_raw = df_svc.create_df()
-        df_clean = cleaner_svc.clean(df_raw)
-
-        rows = upsert_svc.upsert_df("pkmc", df_clean, batch_size)
+        df = BuildPipeline.build_pkmc(raw_svc, cleaner_svc)
+        rows = upsert_svc.upsert_df("pkmc", df, batch_size)
 
         return {
             "message": "Upsert PKMC concluído com sucesso.",
